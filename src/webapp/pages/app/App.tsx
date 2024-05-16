9d292d1c41c1760f2cb3a182b99852f92d1f76cc
Ignacio Foche <nacho.foche@gmail.com>
2024-05-16 14:14:26 +0200

    Initial commit

---
+++ src/webapp/pages/app/App.tsx
@@ -0,0 +1,68 @@
+import { HeaderBar } from "@dhis2/ui";
+import { SnackbarProvider } from "@eyeseetea/d2-ui-components";
+import { Feedback } from "@eyeseetea/feedback-component";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+//@ts-ignore
+import OldMuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import React, { useEffect, useState } from "react";
+import { appConfig } from "../../../app-config";
+import { CompositionRoot } from "../../../CompositionRoot";
+import Share from "../../components/share/Share";
+import { AppContext, AppContextState } from "../../contexts/app-context";
+import { Router } from "../Router";
+import "./App.css";
+import muiThemeLegacy from "./themes/dhis2-legacy.theme";
+import { muiTheme } from "./themes/dhis2.theme";
+
+export interface AppProps {
+    compositionRoot: CompositionRoot;
+}
+
+function App(props: AppProps) {
+    const { compositionRoot } = props;
+    const [showShareButton, setShowShareButton] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [appContext, setAppContext] = useState<AppContextState | null>(null);
+
+    useEffect(() => {
+        async function setup() {
+            const isShareButtonVisible = appConfig.appearance.showShareButton;
+            const currentUser = await compositionRoot.users.getCurrent.execute().toPromise();
+            if (!currentUser) throw new Error("User not logged in");
+
+            setAppContext({ currentUser, compositionRoot });
+            setShowShareButton(isShareButtonVisible);
+            setLoading(false);
+        }
+        setup();
+    }, [compositionRoot]);
+
+    if (loading) return null;
+
+    return (
+        <MuiThemeProvider theme={muiTheme}>
+            <OldMuiThemeProvider muiTheme={muiThemeLegacy}>
+                <SnackbarProvider>
+                    <HeaderBar appName="Skeleton App" />
+
+                    {appConfig.feedback && appContext && (
+                        <Feedback
+                            options={appConfig.feedback}
+                            username={appContext.currentUser.username}
+                        />
+                    )}
+
+                    <div id="app" className="content">
+                        <AppContext.Provider value={appContext}>
+                            <Router />
+                        </AppContext.Provider>
+                    </div>
+
+                    <Share visible={showShareButton} />
+                </SnackbarProvider>
+            </OldMuiThemeProvider>
+        </MuiThemeProvider>
+    );
+}
+
+export default React.memo(App);
