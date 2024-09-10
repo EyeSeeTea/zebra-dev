ceac112bbf1e32e36ff5a58af087b7502b10620c
Ana Garcia <ana@eyeseetea.com>
2024-09-10 15:11:51 +0200

    Add datastore and getProgramIndicatorsFromDatastore common function

---
+++ src/data/repositories/common/getProgramIndicatorsFromDatastore.ts
@@ -0,0 +1,32 @@
+import { Maybe } from "../../../utils/ts-utils";
+import { FutureData } from "../../api-futures";
+import { DataStoreClient } from "../../DataStoreClient";
+
+export enum ProgramIndicatorsDatastoreKey {
+    ActiveVerifiedAlerts = "active-verified-alerts-program-indicators",
+    CasesAlerts = "cases-alerts-program-indicators",
+}
+
+export type ProgramIndicatorsDatastore = {
+    id: string;
+    name: string;
+    disease: string | null;
+    hazardType: string | null;
+    incidentStatus: string | null;
+};
+
+export function getProgramIndicatorsFromDatastore(
+    dataStoreClient: DataStoreClient,
+    programIndicatorsDatastoreKey: ProgramIndicatorsDatastoreKey
+): FutureData<Maybe<ProgramIndicatorsDatastore>> {
+    switch (programIndicatorsDatastoreKey) {
+        case ProgramIndicatorsDatastoreKey.ActiveVerifiedAlerts:
+            return dataStoreClient.getObject<ProgramIndicatorsDatastore>(
+                programIndicatorsDatastoreKey
+            );
+        case ProgramIndicatorsDatastoreKey.CasesAlerts:
+            return dataStoreClient.getObject<ProgramIndicatorsDatastore>(
+                programIndicatorsDatastoreKey
+            );
+    }
+}
