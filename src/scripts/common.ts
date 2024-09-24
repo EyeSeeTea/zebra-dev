e47d3ccc25f98e566de3c2ac7423b376dfb95d37
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2024-09-24 16:54:05 +0100

    feat: setup d2-logger config

--- src/scripts/common.ts
+++ src/scripts/common.ts
@@ -1,4 +1,5 @@
 import { D2Api } from "@eyeseetea/d2-api/2.36";
+import { Instance } from "../data/entities/Instance";
 
 type Auth = {
     username: string;
@@ -14,4 +15,9 @@ export function getD2ApiFromArgs(args: D2ApiArgs): D2Api {
     const { url, auth } = args;
 
     return new D2Api({ baseUrl: url, auth });
+}
+
+export function getInstance(args: D2ApiArgs): Instance {
+    const instance = new Instance({ url: args.url, ...args.auth });
+    return instance;
 }
