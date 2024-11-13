85abec6303d585e10e397fb5be8bffd2d3e54e13
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2024-11-13 13:15:00 +0100

    chore: revert risk assessment button change

--- src/webapp/pages/event-tracker/EventTrackerPage.tsx
+++ src/webapp/pages/event-tracker/EventTrackerPage.tsx
@@ -67,12 +67,6 @@ export const EventTrackerPage: React.FC = React.memo(() => {
         });
     }, [goTo]);
 
-    const goToRiskGradingForm = useCallback(() => {
-        goTo(RouteName.CREATE_FORM, {
-            formType: "risk-assessment-grading",
-        });
-    }, [goTo]);
-
     const { performanceMetrics717, isLoading: _717CardsLoading } = use717Performance(
         "event_tracker",
         id
@@ -133,28 +127,16 @@ export const EventTrackerPage: React.FC = React.memo(() => {
                             {i18n.t("Create Risk Assessment")}
                         </Button>
                     ) : (
-                        <ButtonContainer>
-                            <Button
-                                variant="outlined"
-                                color="secondary"
-                                startIcon={<AddCircleOutline />}
-                                onClick={() => {
-                                    goToRiskSummaryForm();
-                                }}
-                            >
-                                {i18n.t("Add new Assessment")}
-                            </Button>
-                            <Button
-                                variant="outlined"
-                                color="secondary"
-                                startIcon={<AddCircleOutline />}
-                                onClick={() => {
-                                    goToRiskGradingForm();
-                                }}
-                            >
-                                {i18n.t("Add new Grade")}
-                            </Button>
-                        </ButtonContainer>
+                        <Button
+                            variant="outlined"
+                            color="secondary"
+                            startIcon={<AddCircleOutline />}
+                            onClick={() => {
+                                goToRiskSummaryForm();
+                            }}
+                        >
+                            {i18n.t("Add new Assessment")}
+                        </Button>
                     )
                 }
                 titleVariant="secondary"
@@ -261,9 +243,4 @@ export const EventTrackerPage: React.FC = React.memo(() => {
 
 const DurationFilterContainer = styled.div`
     max-width: 250px;
-`;
-
-const ButtonContainer = styled.div`
-    display: flex;
-    gap: 8px;
 `;
