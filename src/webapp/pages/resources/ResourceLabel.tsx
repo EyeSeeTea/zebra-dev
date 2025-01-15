bbb5374b78ae89bcd08190c29167c3c3fe50c456
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2025-01-15 17:04:41 +0100

    feat: add confirmation modal for resource deletion

--- src/webapp/pages/resources/ResourceLabel.tsx
+++ src/webapp/pages/resources/ResourceLabel.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
+import i18n from "../../../utils/i18n";
 import { ResourceBase } from "../../../domain/entities/resources/Resource";
 import { Delete, DescriptionOutlined } from "@material-ui/icons";
 import { Link } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@material-ui/core";
 import { useSnackbar } from "@eyeseetea/d2-ui-components";
 import { useResourceFile } from "./useResourceFile";
 import { Loader } from "../../components/loader/Loader";
+import { SimpleModal } from "../../components/simple-modal/SimpleModal";
 
 interface ResourceLabelProps {
     isDeleting: boolean;
@@ -24,6 +26,7 @@ export const ResourceLabel: React.FC<ResourceLabelProps> = ({
     onDelete,
 }) => {
     const snackbar = useSnackbar();
+    const [openModal, setOpenModal] = useState(false);
     const { resourceFileId, resourceLabel } = resource;
     const { globalMessage, resourceFile, deleteResource } = useResourceFile({
         resourceFileId: resourceFileId,
@@ -54,12 +57,36 @@ export const ResourceLabel: React.FC<ResourceLabelProps> = ({
             </p>
 
             {resourceFileId && userCanDelete && (
-                <div>
-                    {isDeleting && <Loader />}
-                    <Button onClick={() => deleteResource(resourceFileId)}>
+                <>
+                    <Button onClick={() => setOpenModal(true)}>
                         <Delete fontSize="small" color="error" />
                     </Button>
-                </div>
+
+                    <SimpleModal
+                        open={openModal}
+                        onClose={() => setOpenModal(false)}
+                        title={"Delete resource"}
+                        closeLabel={i18n.t("Cancel")}
+                        alignFooterButtons="end"
+                        footerButtons={
+                            <>
+                                {isDeleting && <Loader />}
+                                <StyledButton
+                                    variant="contained"
+                                    onClick={() => deleteResource(resourceFileId)}
+                                >
+                                    {i18n.t("Delete")}
+                                </StyledButton>
+                            </>
+                        }
+                    >
+                        <p>
+                            {i18n.t(
+                                "Are you sure you want to delete this resource? This action cannot be undone."
+                            )}
+                        </p>
+                    </SimpleModal>
+                </>
             )}
         </StyledTemplateLabel>
     );
@@ -77,4 +104,9 @@ const StyledTemplateLabel = styled.div`
         gap: 4px;
         margin: 0;
     }
+`;
+
+const StyledButton = styled(Button)`
+    background-color: ${props => props.theme.palette.error.main};
+    color: ${props => props.theme.palette.common.white};
 `;
