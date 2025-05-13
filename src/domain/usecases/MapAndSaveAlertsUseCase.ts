5d5a6604e85197124ddec714d736085cadae1282
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2025-05-13 18:11:05 +0100

    fix: alert mapping logic in script

--- src/domain/usecases/MapAndSaveAlertsUseCase.ts
+++ src/domain/usecases/MapAndSaveAlertsUseCase.ts
@@ -1,6 +1,6 @@
 import { Future } from "../entities/generic/Future";
 import { Option } from "../entities/Ref";
-import { AlertOptions, AlertRepository } from "../repositories/AlertRepository";
+import { AlertRepository } from "../repositories/AlertRepository";
 import { AlertSyncRepository } from "../repositories/AlertSyncRepository";
 import _ from "../entities/generic/Collection";
 import { DiseaseOutbreakEventBaseAttrs } from "../entities/disease-outbreak-event/DiseaseOutbreakEvent";
@@ -11,7 +11,7 @@ import { OutbreakAlert, OutbreakData } from "../entities/alert/OutbreakAlert";
 import { OutbreakAlertRepository } from "../repositories/OutbreakAlertRepository";
 import { DiseaseOutbreakEventRepository } from "../repositories/DiseaseOutbreakEventRepository";
 import { getOutbreakKey } from "../entities/alert/AlertSynchronizationData";
-import { promiseMap } from "../../utils/promiseMap";
+import { FutureData } from "../../data/api-futures";
 import { ConfigurationsRepository } from "../repositories/ConfigurationsRepository";
 
 export class MapAndSaveAlertsUseCase {
@@ -27,161 +27,177 @@ export class MapAndSaveAlertsUseCase {
         }
     ) {}
 
-    public async execute(): Promise<void> {
-        const { suspectedDiseases } = await this.getOptions();
-        const alertData = await this.options.outbreakAlertRepository.get().toPromise();
+    public execute(): FutureData<void> {
+        return Future.joinObj({
+            alertData: this.options.outbreakAlertRepository.get(),
+        }).flatMap(({ alertData }) => {
+            handleAsyncLogging(
+                logger.info(
+                    `${alertData.length} event(s) found in the Zebra Alerts program with no national event id.`
+                )
+            );
+            const alertsByDisease = getAlertsByDisease(alertData);
 
-        logger.info(
-            `${alertData.length} event(s) found in the Zebra Alerts program with no national event id.`
-        );
-
-        const uniqueFiltersWithAlerts = _(alertData)
-            .groupBy(alert => alert.dataSource)
-            .values()
-            .flatMap(alertsByDataSource => {
-                return getUniqueFilters(alertsByDataSource).map(outbreakData => ({
-                    outbreakData: outbreakData,
-                    alerts: alertsByDataSource,
-                }));
-            });
-
-        await promiseMap(uniqueFiltersWithAlerts, async uniqueFilterWithAlerts => {
-            const { outbreakData, alerts } = uniqueFilterWithAlerts;
-
-            return this.mapDiseaseOutbreakToAlertsAndSave(alerts, outbreakData, suspectedDiseases);
+            return Future.sequential(
+                alertsByDisease.map(diseaseAlerts => {
+                    const { outbreakData, alerts } = diseaseAlerts;
+                    return this.mapDiseaseOutbreakToAlertsAndSave(alerts, outbreakData);
+                })
+            ).flatMap(() => Future.success(undefined));
         });
     }
 
-    private async mapDiseaseOutbreakToAlertsAndSave(
+    private mapDiseaseOutbreakToAlertsAndSave(
         alertData: OutbreakAlert[],
-        outbreakData: OutbreakData,
-        suspectedDiseases: Option[]
-    ): Promise<void> {
-        const diseaseOutbreakEvents = await this.getDiseaseOutbreakEvents(outbreakData);
-
-        if (diseaseOutbreakEvents.length > 1) {
+        outbreakData: OutbreakData
+    ): FutureData<void> {
+        return Future.joinObj({
+            diseaseOutbreakEvents: this.getDiseaseOutbreakEvents(outbreakData),
+            selectableOptions: this.getOptions(),
+        }).flatMap(({ diseaseOutbreakEvents, selectableOptions }) => {
+            const { suspectedDiseases } = selectableOptions;
             const outbreakKey = getOutbreakKey({
                 outbreakValue: outbreakData.value,
                 suspectedDiseases: suspectedDiseases,
             });
 
-            return logger.error(`More than 1 National event found for ${outbreakKey} outbreak.`);
-        }
+            if (diseaseOutbreakEvents.length > 1) {
+                return handleAsyncLogging(
+                    logger.error(`More than 1 National event found for ${outbreakKey} outbreak.`)
+                );
+            }
 
-        const outbreakAlerts = alertData.filter(
-            alertData => alertData.outbreakData.value === outbreakData.value
-        );
+            const outbreakAlerts = alertData.filter(
+                alertData => alertData.outbreakData.value === outbreakData.value
+            );
 
-        await promiseMap(outbreakAlerts, alertData => {
-            return this.mapAndSaveAlertData(alertData, diseaseOutbreakEvents, suspectedDiseases);
+            return Future.sequential(
+                outbreakAlerts.map(alertData =>
+                    this.mapAndSaveAlertData({
+                        alertData: alertData,
+                        diseaseOutbreakEvents: diseaseOutbreakEvents,
+                        outbreakName: outbreakKey,
+                        suspectedDiseases: suspectedDiseases,
+                    })
+                )
+            ).flatMap(() => Future.success(undefined));
         });
     }
 
     private getDiseaseOutbreakEvents(
         outbreakData: OutbreakData
-    ): Promise<DiseaseOutbreakEventBaseAttrs[]> {
-        return this.options.diseaseOutbreakEventRepository
-            .getEventByDisease(outbreakData)
-            .toPromise();
+    ): FutureData<DiseaseOutbreakEventBaseAttrs[]> {
+        return this.options.diseaseOutbreakEventRepository.getEventByDisease(outbreakData);
     }
 
-    private getOptions(): Promise<{ suspectedDiseases: Option[] }> {
+    private getOptions(): FutureData<{ suspectedDiseases: Option[] }> {
         const { configurationsRepository } = this.options;
 
-        return configurationsRepository
-            .getSelectableOptions()
-            .flatMap(selectableOptions => {
-                const { suspectedDiseases } = selectableOptions.eventTrackerConfigurations;
+        return configurationsRepository.getSelectableOptions().flatMap(selectableOptions => {
+            const { suspectedDiseases } = selectableOptions.eventTrackerConfigurations;
 
-                return Future.success({
-                    suspectedDiseases: suspectedDiseases,
-                });
-            })
-            .toPromise();
+            return Future.success({
+                suspectedDiseases: suspectedDiseases,
+            });
+        });
     }
 
-    private mapAndSaveAlertData(
-        alertData: OutbreakAlert,
-        diseaseOutbreakEvents: DiseaseOutbreakEventBaseAttrs[],
-        suspectedDiseases: Option[]
-    ): Promise<void> {
-        const { outbreakData } = alertData;
-
-        const outbreakType = outbreakData.type;
-        const outbreakName = getOutbreakKey({
-            outbreakValue: outbreakData.value,
-            suspectedDiseases: suspectedDiseases,
-        });
+    private mapAndSaveAlertData(options: {
+        alertData: OutbreakAlert;
+        diseaseOutbreakEvents: DiseaseOutbreakEventBaseAttrs[];
+        outbreakName: string;
+        suspectedDiseases: Option[];
+    }): FutureData<void> {
+        const { alertData, diseaseOutbreakEvents, outbreakName, suspectedDiseases } = options;
 
         if (diseaseOutbreakEvents.length === 0) {
-            return this.notifyNationalWatchStaff(alertData, outbreakType, outbreakName);
+            // 3.1. Loop by each alert: first notifyNationalWatchStaff if there is no Zebra national event for that disease.
+            return this.notifyNationalWatchStaff(alertData, outbreakName);
         }
 
         const diseaseOutbreakEvent = diseaseOutbreakEvents[0];
         if (!diseaseOutbreakEvent)
-            return logger.error(
-                `No disease outbreak event found for ${outbreakType} type ${outbreakName}.`
+            return handleAsyncLogging(
+                logger.error(
+                    `No disease outbreak event found for ${outbreakName} type ${outbreakName}.`
+                )
             );
 
         return this.updateAlertData({
-            alertData: alertData,
             diseaseOutbreakEvent: diseaseOutbreakEvent,
+            outbreakData: alertData.outbreakData,
             suspectedDiseases: suspectedDiseases,
         });
     }
 
-    private async notifyNationalWatchStaff(
+    private notifyNationalWatchStaff(
         alertData: OutbreakAlert,
-        alertOutbreakType: string,
         outbreakName: string
-    ): Promise<void> {
-        const { notificationRepository, userGroupRepository } = this.options;
-        logger.debug(`There is no national event with ${outbreakName} ${alertOutbreakType} type.`);
-
-        const userGroup = await userGroupRepository
-            .getUserGroupByCode(RTSL_ZEBRA_NATIONAL_WATCH_STAFF_USER_GROUP_CODE)
-            .toPromise();
-
-        return notificationRepository
-            .notifyNationalWatchStaff(alertData, outbreakName, [userGroup])
-            .toPromise()
-            .then(() => logger.success("Successfully notified all national watch staff."));
+    ): FutureData<void> {
+        return Future.joinObj({
+            logMissingNationalDisease: handleAsyncLogging(
+                logger.debug(`There is no national event with ${outbreakName} disease type.`)
+            ),
+            nationalWatchStaffUserGroup: this.options.userGroupRepository.getUserGroupByCode(
+                RTSL_ZEBRA_NATIONAL_WATCH_STAFF_USER_GROUP_CODE
+            ),
+        }).flatMap(({ nationalWatchStaffUserGroup }) =>
+            this.options.notificationRepository
+                .notifyNationalWatchStaff(alertData, outbreakName, [nationalWatchStaffUserGroup])
+                .flatMap(() => Future.success(undefined))
+        );
     }
 
-    private async updateAlertData(options: {
-        alertData: OutbreakAlert;
+    private updateAlertData(options: {
         diseaseOutbreakEvent: DiseaseOutbreakEventBaseAttrs;
+        outbreakData: OutbreakData;
         suspectedDiseases: Option[];
-    }): Promise<void> {
-        const { alertData, diseaseOutbreakEvent, suspectedDiseases } = options;
+    }): FutureData<void> {
+        const { diseaseOutbreakEvent, outbreakData, suspectedDiseases } = options;
 
-        const alertOptions: AlertOptions = {
-            eventId: diseaseOutbreakEvent.id,
-            outbreakValue: alertData.outbreakData.value,
-        };
-
-        await this.options.alertRepository
-            .updateAlerts(alertOptions)
-            .toPromise()
-            .then(() => logger.success("Successfully updated alert."));
-
-        return this.options.alertSyncRepository
-            .saveAlertSyncData({
-                alert: alertData.alert,
-                outbreakValue: alertOptions.outbreakValue,
-                nationalDiseaseOutbreakEventId: alertOptions.eventId,
-                suspectedDiseases: suspectedDiseases,
+        return this.options.alertRepository
+            .updateAlerts({
+                eventId: diseaseOutbreakEvent.id,
+                outbreakValue: outbreakData.value,
             })
-            .toPromise()
-            .then(() => logger.success("Successfully saved alert sync data."));
+            .flatMap(alerts => {
+                return handleAsyncLogging(logger.success("Successfully updated alert."))
+                    .flatMap(() => {
+                        return Future.sequential(
+                            alerts.map(alert =>
+                                this.options.alertSyncRepository.saveAlertSyncData({
+                                    alert: alert,
+                                    nationalDiseaseOutbreakEventId: diseaseOutbreakEvent.id,
+                                    outbreakValue: outbreakData.value,
+                                    suspectedDiseases: suspectedDiseases,
+                                })
+                            )
+                        ).flatMap(() =>
+                            handleAsyncLogging(
+                                logger.success("Successfully saved alert sync data.")
+                            )
+                        );
+                    })
+                    .flatMap(() => Future.success(undefined));
+            })
+            .flatMap(() => Future.success(undefined));
     }
 }
 
-function getUniqueFilters(alerts: OutbreakAlert[]): OutbreakData[] {
+const handleAsyncLogging = (logger: Promise<void>): FutureData<void> => {
+    return Future.fromPromise(logger).flatMap(() => Future.success(undefined));
+};
+
+function getAlertsByDisease(alerts: OutbreakAlert[]) {
     return _(alerts)
-        .uniqBy(alertData => alertData.outbreakData.value)
-        .map(alertData => alertData.outbreakData)
-        .value();
+        .groupBy(alert => alert.outbreakData.value)
+        .values()
+        .flatMap(alertsByDisease => {
+            return _(alertsByDisease)
+                .uniqBy(alertData => alertData.outbreakData.value)
+                .map(alertData => ({ outbreakData: alertData.outbreakData, alerts: alerts }))
+                .value();
+        });
 }
 
 const RTSL_ZEBRA_NATIONAL_WATCH_STAFF_USER_GROUP_CODE = "RTSL_ZEBRA_NATONAL_WATCH_STAFF";
